3b65f723feb48b23326a6256e2c79ac8849e5a21
Edison Mejia <edison.mejia@sura-am.com>
2024-01-20 10:14:16 -0500

    Initial commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Edison Mejia Estrada on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
